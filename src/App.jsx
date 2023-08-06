8a04854b14763d7dd0510f63e2a8854f235f2309
Shalvin shaji <shalvinshaji283@gmail.com>
2023-08-06 21:09:24 +0530

    started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+function App() {
+  return (
+    <>
+      <div className="app-body">
+        <h2>Bloody soon....</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
