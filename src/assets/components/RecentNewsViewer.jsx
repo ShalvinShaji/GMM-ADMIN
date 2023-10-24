f0a6a00c7ae78ed917e1cd0c4bb13f5d0810f89d
Shalvin shaji <shalvinshaji283@gmail.com>
2023-10-24 23:30:12 +0530

    News viewer page  edited with map function and added custom pop up for upload and deletion

--- src/assets/components/RecentNewsViewer.jsx
+++ src/assets/components/RecentNewsViewer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -8,8 +8,48 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import Sectionhead from "../components/Sectionhead";
 import { Link } from "react-router-dom";
+import CustomPopup from "../components/CustomPopup";
 
 const RecentNewsViewer = () => {
+  const initialNewsData = [
+    {
+      id: 1,
+      title: "News 1",
+      image: "https://images.hdqwalls.com/wallpapers/hellsweeper-vr-sy.jpg",
+      shortDesc:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem fugit nisi consequatur eligendi est doloribus! Adipisci natus quibusdam a doloremque?",
+      date: "August 2023",
+    },
+    {
+      id: 2,
+      title: "News 2",
+      image: "https://images.hdqwalls.com/wallpapers/hellsweeper-vr-sy.jpg",
+      shortDesc:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem fugit nisi consequatur eligendi est doloribus! Adipisci natus quibusdam a doloremque?",
+      date: "August 2023",
+    },
+    // Add more news items as needed
+  ];
+
+  const [newsData, setNewsData] = useState(initialNewsData);
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedNewsId, setSelectedNewsId] = useState(null);
+
+  const handleDeleteNews = (id) => {
+    setSelectedNewsId(id);
+    setShowPopup(true);
+  };
+
+  const handleConfirmDelete = () => {
+    const updatedNews = newsData.filter((News) => News.id !== selectedNewsId);
+    setNewsData(updatedNews);
+    setShowPopup(false);
+  };
+
+  const handlePopupClose = () => {
+    setShowPopup(false);
+  };
+
   return (
     <>
       <section className="Recent-News">
@@ -27,165 +67,47 @@ const RecentNewsViewer = () => {
             </Link>
           </div>
           <div className="row g-2">
-            <div className="col-xl-4 col-md-6  p-3">
-              <div className="post-box p-3">
-                <div className="post-img d-flex justify-content-center">
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/hellsweeper-vr-sy.jpg"
-                    alt="news-img"
-                    className="img-fluid"
-                  />
-                </div>
-                <div className="post-title d-flex justify-content-between">
-                  <p className=" text-wrap">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Delectus, neque.
-                  </p>
-                </div>
-                <div className="post-short-desc">
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Saepe distinctio commodi ad quos consectetur vitae.
-                  </p>
-                </div>
-                <div className="news-details d-flex justify-content-between align-items-center">
-                  <p className="post-date text-end m-0">August 2023</p>
-                  <Button variant="primary" className="image-select-delete-btn">
-                    <span>Read more</span>
-                    <FontAwesomeIcon
-                      icon={faArrowRight}
-                      className="arrow-icon ms-2"
+            {newsData.map((news) => (
+              <div key={news.id} className="col-xl-4 col-md-6  p-3">
+                <div className="post-box p-3">
+                  <div className="post-img d-flex justify-content-center">
+                    <img
+                      src={news.image}
+                      alt="news-img"
+                      className="img-fluid"
                     />
+                  </div>
+                  <div className="post-title d-flex justify-content-between">
+                    <p className=" text-wrap">{news.title}</p>
+                  </div>
+                  <div className="news-post-short-desc">
+                    <p>{news.shortDesc}</p>
+                  </div>
+                  <div className="news-details d-flex justify-content-between align-items-center">
+                    <p className="post-date text-end m-0">{news.date}</p>
+                  </div>
+                </div>
+                <div className="post-delete d-flex justify-content-center align-items-center mt-2 mb-2">
+                  <Button
+                    variant="primary"
+                    className="image-select-delete-btn"
+                    onClick={() => handleDeleteNews(news.id)}
+                  >
+                    <span>Delete Post</span>
+                    <FontAwesomeIcon icon={faTrash} className="ms-2" />
                   </Button>
                 </div>
               </div>
-              <div className="post-delete d-flex justify-content-center align-items-center mt-2 mb-2">
-                <Button variant="primary" className="image-select-delete-btn">
-                  <span>Delete Post</span>
-                  <FontAwesomeIcon icon={faTrash} className="ms-2" />
-                </Button>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6  p-3">
-              <div className="post-box p-3">
-                <div className="post-img d-flex justify-content-center">
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/hellsweeper-vr-sy.jpg"
-                    alt="news-img"
-                    className="img-fluid"
-                  />
-                </div>
-                <div className="post-title d-flex justify-content-between">
-                  <p className=" text-wrap">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Delectus, neque.
-                  </p>
-                </div>
-                <div className="post-short-desc">
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Saepe distinctio commodi ad quos consectetur vitae.
-                  </p>
-                </div>
-                <div className="news-details d-flex justify-content-between align-items-center">
-                  <p className="post-date text-end m-0">August 2023</p>
-                  <Button variant="primary" className="image-select-delete-btn">
-                    <span>Read more</span>
-                    <FontAwesomeIcon
-                      icon={faArrowRight}
-                      className="arrow-icon ms-2"
-                    />
-                  </Button>
-                </div>
-              </div>
-              <div className="post-delete d-flex justify-content-center align-items-center mt-2 mb-2">
-                <Button variant="primary" className="image-select-delete-btn">
-                  <span>Delete Post</span>
-                  <FontAwesomeIcon icon={faTrash} className="ms-2" />
-                </Button>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6  p-3">
-              <div className="post-box p-3">
-                <div className="post-img d-flex justify-content-center">
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/hellsweeper-vr-sy.jpg"
-                    alt="news-img"
-                    className="img-fluid"
-                  />
-                </div>
-                <div className="post-title d-flex justify-content-between">
-                  <p className=" text-wrap">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Delectus, neque.
-                  </p>
-                </div>
-                <div className="post-short-desc">
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Saepe distinctio commodi ad quos consectetur vitae.
-                  </p>
-                </div>
-                <div className="news-details d-flex justify-content-between align-items-center">
-                  <p className="post-date text-end m-0">August 2023</p>
-                  <Button variant="primary" className="image-select-delete-btn">
-                    <span>Read more</span>
-                    <FontAwesomeIcon
-                      icon={faArrowRight}
-                      className="arrow-icon ms-2"
-                    />
-                  </Button>
-                </div>
-              </div>
-              <div className="post-delete d-flex justify-content-center align-items-center mt-2 mb-2">
-                <Button variant="primary" className="image-select-delete-btn">
-                  <span>Delete Post</span>
-                  <FontAwesomeIcon icon={faTrash} className="ms-2" />
-                </Button>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6  p-3">
-              <div className="post-box p-3">
-                <div className="post-img d-flex justify-content-center">
-                  <img
-                    src="https://images.hdqwalls.com/wallpapers/hellsweeper-vr-sy.jpg"
-                    alt="news-img"
-                    className="img-fluid"
-                  />
-                </div>
-                <div className="post-title d-flex justify-content-between">
-                  <p className=" text-wrap">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Delectus, neque.
-                  </p>
-                </div>
-                <div className="post-short-desc">
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Saepe distinctio commodi ad quos consectetur vitae.
-                  </p>
-                </div>
-                <div className="news-details d-flex justify-content-between align-items-center">
-                  <p className="post-date text-end m-0">August 2023</p>
-                  <Button variant="primary" className="image-select-delete-btn">
-                    <span>Read more</span>
-                    <FontAwesomeIcon
-                      icon={faArrowRight}
-                      className="arrow-icon ms-2"
-                    />
-                  </Button>
-                </div>
-              </div>
-              <div className="post-delete d-flex justify-content-center align-items-center mt-2 mb-2">
-                <Button variant="primary" className="image-select-delete-btn">
-                  <span>Delete Post</span>
-                  <FontAwesomeIcon icon={faTrash} className="ms-2" />
-                </Button>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
+      <CustomPopup
+        show={showPopup}
+        onHide={handlePopupClose}
+        onConfirm={handleConfirmDelete}
+        message="Do you really want to delete this News?"
+      />
     </>
   );
 };
