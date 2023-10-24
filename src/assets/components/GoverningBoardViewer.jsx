86599736949a15ec6872e44188fd0b3e4715442a
Shalvin shaji <shalvinshaji283@gmail.com>
2023-10-25 03:08:20 +0530

    governing board details display page alignment corrected

--- src/assets/components/GoverningBoardViewer.jsx
+++ src/assets/components/GoverningBoardViewer.jsx
@@ -35,11 +35,9 @@ const GoverningBoardViewer = () => {
         <div className="row g-2">
           {committeeMembers.map((member, index) => (
             <div key={index} className="col-md-4">
-              <div className="governing-board-member d-flex flex-column justify-content-center align-items-center p-2">
-                <p className="governing-board-member-name mb-1">
-                  {member.name}
-                </p>
+              <div className="governing-board-member d-flex flex-column justify-content-center align-items-center p-3">
                 <p className="governing-board-member-role m-0">{member.role}</p>
+                <p className="governing-board-member-name m-0">{member.name}</p>
               </div>
             </div>
           ))}
