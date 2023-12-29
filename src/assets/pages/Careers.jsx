62bcb83dcba5877d5d1dc3f9eef91cfffe941580
Shalvin shaji <shalvinshaji283@gmail.com>
2023-12-29 18:39:16 +0530

    careers view and upload  page divided into functions

--- src/assets/pages/Careers.jsx
+++ src/assets/pages/Careers.jsx
@@ -6,38 +6,7 @@ import { faSuitcase, faTrash } from "@fortawesome/free-solid-svg-icons";
 import Button from "react-bootstrap/Button";
 import CustomPopup from "../components/CustomPopup";
 
-const initialCareersData = [
-  {
-    id: 1,
-    title: "Career 1",
-    description:
-      "Description for Career 1 nnnnnnnnnnnzdaaaa ajkankfnakfj asjnfkafnlka alkjalfjalf alkkkkkkkkkkkkk",
-    date: "June, 2023",
-    image:
-      "https://images.hdqwalls.com/download/punisher-dark-avenger-65-1538x726.jpg",
-  },
-  {
-    id: 2,
-    title: "Career 2",
-    description:
-      "Description for Career 2 nnnnnnnnnnnzdaaaa ajkankfnakfj asjnfkafnlka alkjalfjalf alkkkkkkkkkkkkk",
-    date: "June, 2023",
-    image:
-      "https://images.hdqwalls.com/download/punisher-dark-avenger-65-1538x726.jpg",
-  },
-  {
-    id: 3,
-    title: "Career 3",
-    description:
-      "Description for Career 3 nnnnnnnnnnnzdaaaa ajkankfnakfj asjnfkafnlka alkjalfjalf alkkkkkkkkkkkkk",
-    date: "June, 2023",
-    image:
-      "https://images.hdqwalls.com/download/punisher-dark-avenger-65-1538x726.jpg",
-  },
-];
-
 const Careers = () => {
-  const [careersData, setCareersData] = useState(initialCareersData);
   const [showPopup, setShowPopup] = useState(false);
   const [selectedCareerId, setSelectedCareerId] = useState(null);
 
@@ -47,16 +16,82 @@ const Careers = () => {
   };
 
   const handleConfirmDelete = () => {
-    const updatedCareers = careersData.filter(
-      (career) => career.id !== selectedCareerId
-    );
-    setCareersData(updatedCareers);
+    console.log("deleted", selectedCareerId);
     setShowPopup(false);
   };
 
   const handlePopupClose = () => {
     setShowPopup(false);
   };
+
+  function careersData() {
+    const initialCareersData = [
+      {
+        id: 1,
+        title: "Career 1",
+        description:
+          "Description for Career 1 nnnnnnnnnnnzdaaaa ajkankfnakfj asjnfkafnlka alkjalfjalf alkkkkkkkkkkkkk",
+        date: "June, 2023",
+        image:
+          "https://images.hdqwalls.com/download/punisher-dark-avenger-65-1538x726.jpg",
+      },
+      {
+        id: 2,
+        title: "Career 2",
+        description:
+          "Description for Career 2 nnnnnnnnnnnzdaaaa ajkankfnakfj asjnfkafnlka alkjalfjalf alkkkkkkkkkkkkk",
+        date: "June, 2023",
+        image:
+          "https://images.hdqwalls.com/download/punisher-dark-avenger-65-1538x726.jpg",
+      },
+      {
+        id: 3,
+        title: "Career 3",
+        description:
+          "Description for Career 3 nnnnnnnnnnnzdaaaa ajkankfnakfj asjnfkafnlka alkjalfjalf alkkkkkkkkkkkkk",
+        date: "June, 2023",
+        image:
+          "https://images.hdqwalls.com/download/punisher-dark-avenger-65-1538x726.jpg",
+      },
+    ];
+
+    return (
+      <div className="row g-2">
+        {initialCareersData.map((career) => (
+          <div key={career.id} className="col-xl-4 col-md-6 p-3">
+            <div className="post-box p-3">
+              <div className="post-img d-flex justify-content-center align-items-center">
+                <img
+                  src={career.image}
+                  alt="career-img"
+                  className="img-fluid"
+                />
+              </div>
+              <div className="post-title d-flex justify-content-between align-items-center">
+                <p className="text-wrap my-1">{career.title}</p>
+              </div>
+              <div className="post-short-desc">
+                <p>{career.description}</p>
+              </div>
+              <div className="post-date">
+                <p className="text-end">{career.date}</p>
+              </div>
+            </div>
+            <div className="post-delete d-flex justify-content-center align-items-center mt-2 mb-2">
+              <Button
+                variant="primary"
+                className="image-select-delete-btn"
+                onClick={() => deleteCareer(career.id)}
+              >
+                <span>Delete Career</span>
+                <FontAwesomeIcon icon={faTrash} className="ms-2" />
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 
   return (
     <>
@@ -76,40 +111,7 @@ const Careers = () => {
               </Link>
             </div>
           </div>
-          <div className="row g-2">
-            {careersData.map((career) => (
-              <div key={career.id} className="col-xl-4 col-md-6 p-3">
-                <div className="post-box p-3">
-                  <div className="post-img d-flex justify-content-center align-items-center">
-                    <img
-                      src={career.image}
-                      alt="career-img"
-                      className="img-fluid"
-                    />
-                  </div>
-                  <div className="post-title d-flex justify-content-between align-items-center">
-                    <p className="text-wrap my-1">{career.title}</p>
-                  </div>
-                  <div className="post-short-desc">
-                    <p>{career.description}</p>
-                  </div>
-                  <div className="post-date">
-                    <p className="text-end">{career.date}</p>
-                  </div>
-                </div>
-                <div className="post-delete d-flex justify-content-center align-items-center mt-2 mb-2">
-                  <Button
-                    variant="primary"
-                    className="image-select-delete-btn"
-                    onClick={() => deleteCareer(career.id)}
-                  >
-                    <span>Delete Career</span>
-                    <FontAwesomeIcon icon={faTrash} className="ms-2" />
-                  </Button>
-                </div>
-              </div>
-            ))}
-          </div>
+          {careersData()}
         </div>
       </section>
       <CustomPopup
